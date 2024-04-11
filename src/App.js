51a08b8c6c3d3f3c36ff5820647ebd0590c23dd8
ashish38dev <ashish38dev>
2024-04-11 15:26:38 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import MarkersMap from './component/MarkersMap';
+
+function App() {
+    return (
+        <div className="App">
+            <MarkersMap />
+        </div>
+    );
+}
+
+export default App;
